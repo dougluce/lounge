60d2965b1b858d143cbefa30c97624b881bd9c10
Doug Luce <doug@github.con.com>
2016-07-16 15:40:17 -0700

    Add a flasher.

--- client/js/lounge.js
+++ client/js/lounge.js
@@ -854,6 +854,74 @@ $(function() {
 		});
 	});
 
+  var alertWindow =  function (title) {
+    if (!this.alertWindowTimer) {
+      this.alertWindowTimer = new (function () {
+        var that = this;
+        var tmr;
+        var has_focus = true;
+        var state = 0;
+        var default_title = "chattin";
+        var title = 'chatin';
+
+        this.setTitle = function (new_title) {
+          new_title = new_title || default_title;
+          window.document.title = new_title;
+          return new_title;
+        };
+
+        this.start = function (new_title) {
+          // Don't alert if we already have focus
+          if (has_focus) return;
+
+          title = new_title;
+          if (tmr) return;
+          tmr = setInterval(this.update, 1000);
+        };
+
+        this.stop = function () {
+          // Stop the timer and clear the title
+          if (tmr) clearInterval(tmr);
+          tmr = null;
+          this.setTitle();
+
+          // Some browsers don't always update the last title correctly
+          // Wait a few seconds and then reset
+          setTimeout(this.reset, 2000);
+        };
+
+        this.reset = function () {
+          if (tmr) return;
+          that.setTitle();
+        };
+
+        this.update = function () {
+          if (state === 0) {
+            that.setTitle(title);
+            state = 1;
+          } else {
+            that.setTitle();
+            state = 0;
+          }
+        };
+
+        $(window).focus(function (event) {
+          has_focus = true;
+          that.stop();
+
+          // Some browsers don't always update the last title correctly
+          // Wait a few seconds and then reset
+          setTimeout(that.reset, 2000);
+        });
+
+        $(window).blur(function (event) {
+          has_focus = false;
+        });
+      })();
+    }
+    this.alertWindowTimer.start(title);
+  }
+
 	chat.on("msg", ".messages", function(e, target, msg) {
 		if (msg.self) {
 			return;
@@ -882,7 +950,7 @@ $(function() {
 						title += " says:";
 						body = msg.text.replace(/\x02|\x1D|\x1F|\x16|\x0F|\x03(?:[0-9]{1,2}(?:,[0-9]{1,2})?)?/g, "").trim();
 					}
-
+          alertWindow(body);
 					var notify = new Notification(title, {
 						body: body,
 						icon: "img/logo-64.png",
